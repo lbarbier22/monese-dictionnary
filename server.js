135a645ef6dbb42f0f0bd81a883480ee0ce6d5ec
lbarbier22 <barbier.lucas08@gmail.com>
2025-07-11 08:47:16 +0200

    feat : ajout du serverside et les routes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const mots = require('./ressources/rare_words.json');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'vues'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const mot = mots[Math.floor(Math.random() * mots.length)];
+    res.redirect(`/mot/${encodeURIComponent(mot.Mot)}`);
+});
+
+app.get('/liste', (req, res) => {
+    const motsTries = [...mots].sort((a, b) => a.Mot.localeCompare(b.Mot));
+    res.render('index', { mots: motsTries });
+});
+
+app.get('/mot/:nom', (req, res) => {
+    const nom = req.params.nom.toLowerCase();
+    const mot = mots.find(m => m.Mot.toLowerCase() === nom);
+    if (mot) {
+        res.render('mot', { mot });
+    } else {
+        res.status(404).send('Mot introuvable');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur lancé : http://localhost:${PORT}`);
+});
